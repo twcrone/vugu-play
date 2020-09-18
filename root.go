c28123bff2be9ca91478e4a91d4d6301a7b725fc
Todd Crone <twcrone@gmail.com>
2020-09-18 18:09:47 -0400

    Add button to fetch love notes

--- root.go
+++ root.go
@@ -1,19 +1,58 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/vugu/vugu"
-	"log"
-	_ "time"
 )
+import "net/http"
+import "log"
 
 type Root struct {
-	Show     bool `vugu:"data"`
-	ShowWasm bool `vugu:"data"`
-	ShowGo   bool `vugu:"data"`
-	ShowVugu bool `vugu:"data"`
+	Resp      resp `vugu:"data"`
+	IsLoading bool `vugu:"data"`
 }
 
-func (c *Root) Toggle(e vugu.DOMEvent) {
-	c.Show = !c.Show
-	log.Printf("Toggled! Show is now %t", c.Show)
+type resp struct {
+	Data []note `json:"data"`
+}
+
+type note struct {
+	Id      int    `json:"id"`
+	Message string `json:"message"`
+}
+
+func (c *Root) HandleClick(event vugu.DOMEvent) {
+	fmt.Println("HELLO")
+	//c.Notes
+
+	ee := event.EventEnv()
+
+	go func() {
+
+		ee.Lock()
+		c.IsLoading = true
+		ee.UnlockRender()
+
+		res, err := http.Get("https://go-love-notes.herokuapp.com/notes")
+		fmt.Println(res.Body)
+		if err != nil {
+			log.Printf("Error fetch()ing: %v", err)
+			return
+		}
+		defer res.Body.Close()
+
+		resp := resp{}
+		err = json.NewDecoder(res.Body).Decode(&resp)
+		if err != nil {
+			log.Printf("Error JSON decoding: %v", err)
+			return
+		}
+
+		ee.Lock()
+		defer ee.UnlockRender()
+		c.Resp = resp
+		c.IsLoading = false
+
+	}()
 }
